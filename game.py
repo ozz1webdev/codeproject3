5b30a76efb9e09c18243b82853161ef51e8baaa9
ozz1webdev <ozz1webdev@gmail.com>
2024-04-04 19:24:47 +0200

    add checkWin function

--- game.py
+++ game.py
@@ -18,7 +18,81 @@ def setMark(boardSel,square,mark):
     print(word_art.logo)
     #print(boardSel)
     return boardSel
-
+def checkWin(squareVal):
+    if squareVal[1] == squareVal[2] and squareVal[2] == squareVal[3]:
+        if squareVal[1] == 'X':
+            gf.typingPrint(word_art.you_win)
+            time.sleep(1)
+            return 'win'
+        elif squareVal[1] == 'O':
+            gf.typingPrint(word_art.you_lose)
+            time.sleep(1)
+            return 'lose'
+    elif squareVal[4] == squareVal[5] and squareVal[5] == squareVal[6]:
+        if squareVal[4] == 'X':
+            gf.typingPrint(word_art.you_win)
+            time.sleep(1)
+            return 'win'
+        elif squareVal[4] == 'O':
+            gf.typingPrint(word_art.you_lose)
+            time.sleep(1)
+            return 'lose'
+    elif squareVal[7] == squareVal[8] and squareVal[8] == squareVal[9]:
+        if squareVal[7] == 'X':
+            gf.typingPrint(word_art.you_win)
+            time.sleep(1)
+            return 'win'
+        elif squareVal[7] == 'O':
+            gf.typingPrint(word_art.you_lose)
+            time.sleep(1)
+            return 'lose'
+    elif squareVal[1] == squareVal[4] and squareVal[4] == squareVal[7]:
+        if squareVal[1] == 'X':
+            gf.typingPrint(word_art.you_win)
+            time.sleep(1)
+            return 'win'
+        elif squareVal[1] == 'O':
+            gf.typingPrint(word_art.you_lose)
+            time.sleep(1)
+            return 'lose'
+    elif squareVal[2] == squareVal[5] and squareVal[5] == squareVal[8]:
+        if squareVal[2] == 'X':
+            gf.typingPrint(word_art.you_win)
+            time.sleep(1)
+            return 'win'
+        elif squareVal[2] == 'O':
+            gf.typingPrint(word_art.you_lose)
+            time.sleep(1)
+            return 'lose'
+    elif squareVal[3] == squareVal[6] and squareVal[6] == squareVal[9]:
+        if squareVal[3] == 'X':
+            gf.typingPrint(word_art.you_win)
+            time.sleep(1)
+            return 'win'
+        elif squareVal[3] == 'O':
+            gf.typingPrint(word_art.you_lose)
+            time.sleep(1)
+            return 'lose'
+    elif squareVal[1] == squareVal[5] and squareVal[5] == squareVal[9]:
+        if squareVal[1] == 'X':
+            gf.typingPrint(word_art.you_win)
+            time.sleep(1)
+            return 'win'
+        elif squareVal[1] == 'O':
+            gf.typingPrint(word_art.you_lose)
+            time.sleep(1)
+            return 'lose'
+    elif squareVal[3] == squareVal[5] and squareVal[5] == squareVal[7]:
+        if squareVal[3] == 'X':
+            gf.typingPrint(word_art.you_win)
+            time.sleep(1)
+            return 'win'
+        elif squareVal[3] == 'O':
+            gf.typingPrint(word_art.you_lose)
+            time.sleep(1)
+            return 'lose'
+    else:
+        return 'continue'
 def compTurn(boardSel):
     print("compTurn func")
     boardSel = setMark(boardSel,1,'O')
