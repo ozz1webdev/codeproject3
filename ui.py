ba664765f48faaf31fd7c75ff0477426cfa9f14d
ozz1webdev <ozz1webdev@gmail.com>
2024-04-04 10:32:58 +0200

    adding menu and rules

--- ui.py
+++ ui.py
@@ -1,9 +1,23 @@
 menu = '''
 ╔═════════TIC-TAC-TOE MENU══════════╗
-║░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░║
-║░░░░░░░░░░1: Start Game░░░░░░░░░░░░║
-║░░░░░░░░░░2: How To Play░░░░░░░░░░░║
-║░░░░░░░░░░3: Exit Game░░░░░░░░░░░░░║
-║░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░║
+║                                   ║
+║          1: Start Game            ║
+║          2: How To Play           ║
+║          3: Exit Game             ║
+║                                   ║
 ╚═══════════════════════════════════╝
+'''
+
+rules = '''
+╔══════════════════════════════════════TIC-TAC-TOE RULES══════════════════════════════════════════╗
+║                                                                                                 ║
+║    1. The game is played on a grid that's 3 squares by 3 squares.                               ║
+║    2. You are X, your friend (or the computer in this case) is O.                               ║
+║       Players take turns putting their marks in empty squares.                                  ║
+║    3. The first player to get 3 of her marks in a row (vertically, horizontally, or diagonally) ║
+║       is the winner.                                                                            ║
+║    4. When all 9 squares are full, the game is over. If no player has 3 marks in a row,         ║
+║       the game ends in a tie.                                                                   ║
+║                                                                                                 ║
+╚═════════════════════════════════════════════════════════════════════════════════════════════════╝
 '''
